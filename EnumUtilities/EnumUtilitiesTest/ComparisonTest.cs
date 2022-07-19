c7846bd53aeb2ae441e88869747532204cd36120
Richard Munson <108591053+rmunson27@users.noreply.github.com>
2022-07-19 18:18:30 -0400

    Added comparison testing

---
+++ EnumUtilities/EnumUtilitiesTest/ComparisonTest.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rem.CoreTest.Utilities;
+
+/// <summary>
+/// Tests the comparison methods of the <see cref="Enums"/> class.
+/// </summary>
+[TestClass]
+public class ComparisonTest
+{
+    /// <summary>
+    /// Tests all comparison methods.
+    /// </summary>
+    [TestMethod]
+    public void TestComparisons()
+    {
+        foreach (var value1 in NonBitSets.AllValues)
+        {
+            foreach (var value2 in NonBitSets.AllValues)
+            {
+                Assert.AreEqual(value1 < value2, Enums.Less(value1, value2));
+                Assert.AreEqual(value1 == value2, Enums.Equal(value1, value2));
+                Assert.AreEqual(value1 > value2, Enums.Greater(value1, value2));
+                Assert.AreEqual(value1 <= value2, Enums.LessOrEqual(value1, value2));
+                Assert.AreEqual(value1 >= value2, Enums.GreaterOrEqual(value1, value2));
+                Assert.AreEqual(value1.CompareTo(value2), Enums.CompareTo(value1, value2));
+            }
+        }
+    }
+}
