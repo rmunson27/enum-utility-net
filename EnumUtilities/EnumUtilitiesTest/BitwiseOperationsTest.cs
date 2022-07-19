a2ff45cf72d08f53e8f1d7bf4d862a162adf6d1a
Richard Munson <108591053+rmunson27@users.noreply.github.com>
2022-07-19 15:50:09 -0400

    Added testing for the `Enums` classs bitwise operations methods

---
+++ EnumUtilities/EnumUtilitiesTest/BitwiseOperationsTest.cs
@@ -0,0 +1,87 @@
+namespace Rem.CoreTest.Utilities;
+
+/// <summary>
+/// Tests of the <see cref="Enums"/> class bitwise operations.
+/// </summary>
+[TestClass]
+public class BitwiseOperationsTest
+{
+    /// <summary>
+    /// Tests the <see cref="Enums.And{TEnum}(TEnum, TEnum)"/> method.
+    /// </summary>
+    [TestMethod]
+    public void TestAnd()
+    {
+        foreach (var value1 in BitSet_Defaults.AllValues)
+        {
+            foreach (var value2 in BitSet_Defaults.AllValues)
+            {
+                Assert.AreEqual(value1 & value2, Enums.And(value1, value2));
+            }
+        }
+    }
+
+    /// <summary>
+    /// Tests the <see cref="Enums.Or{TEnum}(TEnum, TEnum)"/> method.
+    /// </summary>
+    [TestMethod]
+    public void TestOr()
+    {
+        foreach (var value1 in BitSet_Defaults.AllValues)
+        {
+            foreach (var value2 in BitSet_Defaults.AllValues)
+            {
+                Assert.AreEqual(value1 | value2, Enums.Or(value1, value2));
+            }
+        }
+    }
+
+    /// <summary>
+    /// Tests the <see cref="Enums.Or{TEnum}(TEnum[])"/> method.
+    /// </summary>
+    /// <remarks>
+    /// The <see cref="Enums.Or{TEnum}(IEnumerable{TEnum})"/> method uses the same underlying code.
+    /// </remarks>
+    [TestMethod]
+    public void TestOrRange()
+    {
+        foreach (var value1 in BitSet_Defaults.AllValues)
+        {
+            foreach (var value2 in BitSet_Defaults.AllValues)
+            {
+                Assert.AreEqual(value1 | value2, Enums.Or(new[] { value1, value2 }));
+            }
+        }
+
+        Assert.AreEqual(
+            BitSet_Default.One | BitSet_Default.Two | BitSet_Default.Four,
+            Enums.Or(new[] { BitSet_Default.One, BitSet_Default.Two, BitSet_Default.Four }));
+    }
+
+    /// <summary>
+    /// Tests the <see cref="Enums.XOr{TEnum}(TEnum, TEnum)"/> method.
+    /// </summary>
+    [TestMethod]
+    public void TestXOr()
+    {
+        foreach (var value1 in BitSet_Defaults.AllValues)
+        {
+            foreach (var value2 in BitSet_Defaults.AllValues)
+            {
+                Assert.AreEqual(value1 ^ value2, Enums.XOr(value1, value2));
+            }
+        }
+    }
+
+    /// <summary>
+    /// Tests the <see cref="Enums.Not{TEnum}(TEnum)"/> method.
+    /// </summary>
+    [TestMethod]
+    public void TestNot()
+    {
+        foreach (var value in BitSet_Defaults.AllValues)
+        {
+            Assert.AreEqual(~value, Enums.Not(value));
+        }
+    }
+}
---
+++ EnumUtilities/EnumUtilitiesTest/TestTypes.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rem.CoreTest.Utilities;
+
+internal static class BitSet_Defaults
+{
+    public static readonly ImmutableArray<BitSet_Default> AllAtomicValues = ImmutableArray.CreateRange(new[]
+    {
+        BitSet_Default.One,
+        BitSet_Default.Two,
+        BitSet_Default.Four,
+    });
+
+    public static readonly ImmutableArray<BitSet_Default> AllValues = ImmutableArray.CreateRange(new[]
+    {
+        BitSet_Default.Zero,
+
+        BitSet_Default.One,
+        BitSet_Default.Two,
+        BitSet_Default.Four,
+
+        BitSet_Default.One | BitSet_Default.Two,
+        BitSet_Default.One | BitSet_Default.Four,
+        BitSet_Default.Two | BitSet_Default.Four,
+
+        BitSet_Default.One | BitSet_Default.Two | BitSet_Default.Four,
+    });
+}
+
+[Flags]
+internal enum BitSet_NoDefault : byte
+{
+    One = 1,
+    Two = 2,
+    Four = 4,
+
+    Three = One | Two,
+}
+
+[Flags]
+internal enum BitSet_Default : byte
+{
+    Zero = 0,
+    One = 1,
+    Two = 2,
+    Four = 4,
+}
+
+internal enum NonBitSet : short
+{
+    Zero = 0,
+    One = 1,
+    Two = 2,
+    Three = 3,
+}
